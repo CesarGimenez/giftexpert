89c533aa098ce3bedf28c5e047e386da7142fddb
Cesar Gimenez <cesargimenez760@gmail.com>
2021-09-14 16:21:34 -0400

    update app

--- src/GiftExpert.js
+++ src/GiftExpert.js
@@ -5,12 +5,10 @@ import { GridGifts } from "./components/GridGifts";
 export const GiftExpert = () => {
   const [categories, setCategories] = useState([""]);
 
-  const AddCategor = () => setCategories([...categories, "Inu Yasha"]);
   return (
     <div>
       <h2>GiftExpert</h2>
       <hr />
-      <button onClick={AddCategor}>Buscar</button>
       <AddCategory setCategories={setCategories} />
 
       {categories.map((category) => {
